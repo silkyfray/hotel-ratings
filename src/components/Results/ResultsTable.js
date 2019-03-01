f56ea197a974303156d2f7020f50e02eee0700db
Antonio Nikolov <anikolovn@gmail.com>
2019-03-01 15:14:42 +0000

    Implemented basic table

--- src/components/Results/ResultsTable.js
+++ src/components/Results/ResultsTable.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Table from 'react-bootstrap/Table';
 
 const ResultsTable = ({ hotels, sort, facilities }) => {
-	return <div />;
+	return (
+		<Table responsive className="Results">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Stars</th>
+					<th>Facilities</th>
+				</tr>
+			</thead>
+			<tbody>
+				{hotels.map((hotel, index) => {
+					return (
+						<tr className="results-row" key={index}>
+							<td>{hotel.name}</td>
+							<td>{hotel.starRating}</td>
+							<td>{hotel.facilities}</td>
+						</tr>
+					);
+				})}
+			</tbody>
+		</Table>
+	);
 };
 
 ResultsTable.propTypes = {
