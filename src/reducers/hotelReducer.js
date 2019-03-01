f060846931d50988ce03b6e078eda7e4b86ead11
Antonio Nikolov <anikolovn@gmail.com>
2019-03-01 14:38:50 +0000

    implement add facilities filter reducer

--- src/reducers/hotelReducer.js
+++ src/reducers/hotelReducer.js
@@ -9,11 +9,16 @@ export const initialState = {
 
 export default (state = initialState, action) => {
 	switch (action.type) {
-		case hotelActionTypes.ADD_HOTEL:
+		case hotelActionTypes.ADD_HOTEL: {
 			const result = { ...state };
 			result.hotels.push(action.payload);
 			return result;
-		case hotelActionTypes.ADD_FACILITY_FILTER:
+		}
+		case hotelActionTypes.ADD_FACILITY_FILTER: {
+			const result = { ...state };
+			result.facilities.push(action.payload);
+			return result;
+		}
 		case hotelActionTypes.REMOVE_FACILITY_FILTER:
 		case hotelActionTypes.SORT_ASC:
 		case hotelActionTypes.SORT_DESC:
